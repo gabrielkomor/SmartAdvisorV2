6460d85074d54be7c5ffae2f428a96b8e4fbc968
Gabriel Komor <gabkomti@gmail.com>
2026-06-30 16:24:58 +0200

    add history decisions chart

--- frontend/src/pages/SignalsHistory.tsx
+++ frontend/src/pages/SignalsHistory.tsx
@@ -1,7 +1,68 @@
 import type { JSX } from "react";
+import { Chart } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Tooltip,
+  Legend,
+);
 
 const SignalsHistory = (): JSX.Element => {
-  return <h1>Signals history</h1>;
+  const data = {
+    labels: [1, 2, 3, 4, 5],
+    datasets: [
+      {
+        label: "Buy",
+        data: [40, 50, 20, 10, 90],
+        borderColor: "rgba(0, 160, 0, 1)",
+        backgroundColor: "rgba(0, 160, 0, 0.3)",
+        tension: 0.3,
+      },
+      {
+        label: "Sell",
+        data: [20, 30, 0, 70, 0],
+        borderColor: "rgba(210, 0, 0, 1)",
+        backgroundColor: "rgba(210, 0, 0, 0.3)",
+        tension: 0.3,
+      },
+      {
+        label: "Hold",
+        data: [40, 20, 80, 20, 10],
+        borderColor: "rgba(120, 120, 120, 1)",
+        backgroundColor: "rgba(120, 120, 120, 0.3)",
+        tension: 0.3,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        min: 0,
+        max: 100,
+      },
+    },
+  };
+
+  return (
+    <div className="h-full">
+      <Chart type="line" data={data} options={options} />
+    </div>
+  );
 };
 
 export default SignalsHistory;
