3684a994c270e1dea098345d4974777aa8040488
Gabriel Komor <gabkomti@gmail.com>
2026-07-02 14:45:46 +0200

    add new themes and app state

--- frontend/src/pages/AppSettings.tsx
+++ frontend/src/pages/AppSettings.tsx
@@ -1,7 +1,91 @@
 import type { JSX } from "react";
 
 export const AppSettings = (): JSX.Element => {
-  return <h1>Settings</h1>;
+  return (
+    <fieldset className="fieldset">
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="corporate"
+        />
+        Corporate
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="retro"
+        />
+        Retro
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="cyberpunk"
+        />
+        Cyberpunk
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="valentine"
+        />
+        Valentine
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="aqua"
+        />
+        Aqua
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="dark"
+        />
+        Dark
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="black"
+        />
+        Black
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="dim"
+        />
+        Dim
+      </label>
+      <label className="flex gap-2 cursor-pointer items-center">
+        <input
+          type="radio"
+          name="theme-radios"
+          className="radio radio-sm theme-controller radio-primary"
+          value="light"
+        />
+        Light
+      </label>
+    </fieldset>
+  );
 };
 
 export default AppSettings;
