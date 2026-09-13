b03ef1de97e984e0ff50a96eaac218673aa21519
gabrielkomor <gabkomti@gmail.com>
2026-09-13 18:14:18 +0200

    added docker files content, compose file and command for local cicd

--- backend/src/app.py
+++ backend/src/app.py
@@ -23,6 +23,8 @@ app.add_middleware(
         "http://127.0.0.1:8000",
         "http://192.168.1.111:5173",
         "http://192.168.1.111:5174",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
